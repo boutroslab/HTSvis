5eb2c7c8445069d59aa61ec2d7878558d16d3a10
Christian Scheeder <christian.scheeder@gmx.de>
2016-12-22 22:30:30 +0100

    Description added

--- inst/appdir/DropDownInput.R
+++ inst/appdir/DropDownInput.R
@@ -1,3 +1,4 @@
+#### Function to define specific drop down lists on 'data input' tab via renderUI 
 DropDownInput <- function(identifier,input,label,multiState){
   return(
     renderUI({
@@ -5,10 +6,10 @@ DropDownInput <- function(identifier,input,label,multiState){
       
       if(!isTRUE(multiState) )     
         selectizeInput(identifier,
-                    label = label,
-                    choices = input,
-                    multiple = T,
-                    options = list(maxItems =1, placeholder = 'not defined'))
+                      label = label,
+                      choices = input,
+                      multiple = T,
+                      options = list(maxItems =1, placeholder = 'not defined'))
         else 
           selectizeInput(identifier,
                          label = label,
