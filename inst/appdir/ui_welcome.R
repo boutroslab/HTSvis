083f6af8cc6001fa18a1c7788086f960eeecdf58
Christian Scheeder <c.scheeder@dkfz.de>
2017-04-26 15:23:32 +0200

    final edits before release

--- inst/appdir/ui_welcome.R
+++ inst/appdir/ui_welcome.R
@@ -7,7 +7,7 @@ fixedRow(
                      # HTML("</div>"),
                      column(12,
                             align = "left",
-                            h2(paste0("HTSvis: a web app for exploratory data analysis",
+                            h2(paste0("HTSvis: A web app for exploratory data analysis",
                                       "and visualization of arrayed high-throughput screens")),
                             HTML("<div style='height: 25px;'>"),
                             HTML("</div>"),
